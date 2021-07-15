9920fc983b98a80c0eeac2895b39692774f0bd48
Braydon Whitfield <whitfield17@gmail.com>
2021-07-15 15:14:25 -0400

    - Added CodeTextView - Display video descriptions as a TextView using html styling

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Braydon Whitfield on 2021-07-15.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+}
---
+++ LearningApp/Views/CodeTextView.swift
@@ -0,0 +1,36 @@
+//
+//  CodeTextView.swift
+//  LearningApp
+//
+//  Created by Braydon Whitfield on 2021-07-15.
+//
+
+import SwiftUI
+
+struct CodeTextView: UIViewRepresentable {
+        
+    @EnvironmentObject var model: ContentModel
+    
+    func makeUIView(context: Context) -> UITextView {
+        
+        let textView = UITextView()
+        textView.isEditable = false
+        
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        
+        //Set the attributed text for the lesson
+        uiView.attributedText = model.lessonDescription
+        
+        //Scroll back to the top
+        uiView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: false)
+    }
+}
+
+struct CodeTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        CodeTextView()
+    }
+}
